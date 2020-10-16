9f5b6d9d90d048abd23db6f144a60a8fa22cf11a
rohithsai209@gmail.com <rohith@209>
2020-10-16 20:02:32 +0530

    2nd Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductsListComponent } from './components/shopping-cart/products-list/products-list.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { CartListComponent } from './components/shopping-cart/cart/cart-list/cart-list.component';
+import { ProductsItemComponent } from './components/shopping-cart/products-list/products-item/products-item.component';
+import { ProductComponent } from './models/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductsListComponent,
+    CartComponent,
+    CartListComponent,
+    ProductsItemComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
